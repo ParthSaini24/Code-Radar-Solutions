ab322ab80e8fb01c0e395b809fa6f9dbcd54e02e
ParthSaini24 <parth24saini87@gmail.com>
2025-02-01 21:50:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -19,7 +19,7 @@ int main() {
     {
         if(b==0)
         {
-            printf("Error\n");
+            printf("error\n");
         }
         else
         {
@@ -28,7 +28,7 @@ int main() {
     }
     else
     {
-        printf("Error\n");
+        printf("error\n");
     }
     return 0;
 }
